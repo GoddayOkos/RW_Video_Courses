41eb8e78d49f92b1976f28b8b74675f1a2cc44a1
Godday Okoduwa <goddayokos@gmail.com>
2021-09-22 16:24:22 +0100

    Added http package to the project

--- lib/repository/course_repository.dart
+++ lib/repository/course_repository.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 import 'package:rw_courses/constants.dart';
 import 'package:rw_courses/model/course.dart';
 import 'package:rw_courses/repository/repository.dart';
@@ -15,6 +18,11 @@ class CourseRepository implements Repository {
     if (domainFilter != Constants.allFilter) {
       url += "&filter[domain_ids][]=$domainFilter";
     }
+
+    http.Response response = await http.get(url);
+    final apiResponse = json.decode(response.body);
+
+    print(apiResponse);
 
     return courses;
   }
