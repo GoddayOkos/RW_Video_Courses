ecef70726d5b76b6dbc6b1ec9183b23229735aca
Godday Okoduwa <goddayokos@gmail.com>
2021-09-22 12:47:57 +0100

    Added the course model

---
+++ lib/model/course.dart
@@ -0,0 +1,11 @@
+class Course {
+  final String courseId;
+  final String name;
+  final String description;
+
+  Course({
+    required this.courseId,
+    required this.name,
+    required this.description
+});
+}
