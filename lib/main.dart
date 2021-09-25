8907c58ab2e017839df060cb7527a5f761668b87
Godday Okoduwa <goddayokos@gmail.com>
2021-09-26 00:31:08 +0100

    Lifted up the state from the Widgets to their parent Container

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:rw_courses/rwcourses_app.dart';
+import 'package:rw_courses/state/filter_state_container.dart';
 import 'package:rw_courses/strings.dart';
 
-void main() => runApp(MaterialApp(
-      title: Strings.appTitle,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          primaryColor: Colors.green.shade800),
-      home: const RWCoursesApp(),
+void main() => runApp(FilterStateContainer(
+      child: MaterialApp(
+        title: Strings.appTitle,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primaryColor: Colors.green.shade800),
+        home: const RWCoursesApp(),
+      ),
     ));
