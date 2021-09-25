dc4bac4843b4446c8ba42820faf571574e8fee66
Godday Okoduwa <goddayokos@gmail.com>
2021-09-25 10:51:24 +0100

    Created an enum class for parsing responses domain and an extension method for getting the appropriate string

---
+++ lib/model/domain.dart
@@ -0,0 +1,36 @@
+import 'package:rw_courses/strings.dart';
+
+enum Domain {
+  all,
+  ios,
+  android,
+  flutter,
+  sss,
+  unity,
+  macos,
+  archived,
+  unknown
+}
+
+extension DomainExtension on Domain {
+  String get name {
+    switch(this) {
+      case Domain.ios:
+        return Strings.ios;
+      case Domain.android:
+        return Strings.android;
+      case Domain.unity:
+        return Strings.unity;
+      case Domain.sss:
+        return Strings.sss;
+      case Domain.flutter:
+        return Strings.flutter;
+      case Domain.macos:
+        return Strings.macos;
+      case Domain.archived:
+        return Strings.archived;
+      default:
+        return Strings.unknown;
+    }
+  }
+}
