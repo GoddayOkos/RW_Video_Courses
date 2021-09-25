8b86f406b9471e2e36464d0bb47c3ce0c5b1dd85
Godday Okoduwa <goddayokos@gmail.com>
2021-09-25 17:40:12 +0100

    Displayed filter options usingthe filter widget

--- lib/ui/filter_course/filter_page.dart
+++ lib/ui/filter_course/filter_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:rw_courses/strings.dart';
+import 'package:rw_courses/ui/filter_course/filter_widget.dart';
+
+import '../../constants.dart';
 
 class FilterPage extends StatefulWidget {
   const FilterPage({Key? key}) : super(key: key);
@@ -9,17 +12,69 @@ class FilterPage extends StatefulWidget {
 }
 
 class _FilterPageState extends State<FilterPage> {
+ int _filterValue = Constants.allFilter;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(Strings.filter),
-        backgroundColor: Colors.green.shade800,
+          backgroundColor: Colors.green.shade800
       ),
-      body: const Center(
-        child: Text("Filter page placeholder!")
-      )
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          FilterWidget(
+            value: Constants.iosFilter,
+            groupValue: _filterValue,
+            onChanged: _handleRadioValueChange,
+            text: Strings.ios,
+          ),
+          FilterWidget(
+            value: Constants.androidFilter,
+            groupValue: _filterValue,
+            onChanged: _handleRadioValueChange,
+            text: Strings.android,
+          ),
+          FilterWidget(
+            value: Constants.sssFilter,
+            groupValue: _filterValue,
+            onChanged: _handleRadioValueChange,
+            text: Strings.sss,
+          ),
+          FilterWidget(
+            value: Constants.unityFilter,
+            groupValue: _filterValue,
+            onChanged: _handleRadioValueChange,
+            text: Strings.unity,
+          ),
+          FilterWidget(
+            value: Constants.macosFilter,
+            groupValue: _filterValue,
+            onChanged: _handleRadioValueChange,
+            text: Strings.macos,
+          ),
+          FilterWidget(
+            value: Constants.flutterFilter,
+            groupValue: _filterValue,
+            onChanged: _handleRadioValueChange,
+            text: Strings.flutter,
+          ),
+          FilterWidget(
+            value: Constants.allFilter,
+            groupValue: _filterValue,
+            onChanged: _handleRadioValueChange,
+            text: Strings.all,
+          )
+        ],
+      ),
     );
+  }
+
+  void _handleRadioValueChange(int? value) {
+    setState(() {
+      _filterValue = value!;
+    });
   }
 
 }
